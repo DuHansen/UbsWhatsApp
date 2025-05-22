c766c0434339479bd85550112fdf648a1ead4528
Eduardo Hansen <101521623+DuHansen@users.noreply.github.com>
2025-05-22 13:28:36 -0300

    Update whatsapp.js

--- whatsauto.com.bt/api/whatsapp.js
+++ whatsauto.com.bt/api/whatsapp.js
@@ -62,17 +62,17 @@ class WhatsAppAPI {
     }
 
     async handleMessage(message) {
-        
         try {
             const lowerCaseMessage = message.body.toLowerCase();
             if (message.type === 'ptt') {
                 console.log('Recebido um áudio (ptt)!');
-                    await this.client.sendMessage(message.from, 
-                        '*Atendimento SUS:*\n\n' +
-                        'Pedimos a gentileza de nos enviar sempre por escrito a sua necessidade. Aguardamos para seguir.' +
-                        '\nObrigado!'
-                    );     
+                await this.client.sendMessage(message.from, 
+                    '*Atendimento SUS:*\n\n' +
+                    'Pedimos a gentileza de nos enviar sempre por escrito a sua necessidade. Aguardamos para seguir.' +
+                    '\nObrigado!'
+                );     
             }
+            
             if (!this.botAtivado) {
                 return;
             }
@@ -82,7 +82,7 @@ class WhatsAppAPI {
             }
     
             const state = this.userState[message.from];
-
+    
             if (state === 'inicio') {
                 await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' + 'Olá! Como posso te ajudar? Escolha uma opção:\n1. Dúvidas\n2. Atendimento');
                 this.userState[message.from] = 'aguardando_opcao';
@@ -93,7 +93,7 @@ class WhatsAppAPI {
                 if (lowerCaseMessage.includes('1') || lowerCaseMessage.includes('dúvidas')) {
                     this.userState[message.from] = 'duvidas';
                     await this.client.sendMessage(message.from, 
-                        '*Atendimento SUS:*\n\n' + 'Escolha uma dúvida:\n1. Quando vou receber meu prêmio?\n2. Posso receber o dinheiro no lugar do prêmio?\n3. Como posso garantir que não é um golpe?\n4. Atendimento (Se preferir falar com um atendente)'
+                        '*Atendimento SUS:*\n\n' + 'Escolha uma dúvida:\n1. Como agendar uma consulta no SUS?\n2. Quais são os documentos necessários para ser atendido?\n3. Posso ser atendido em qualquer unidade do SUS?\n4. Como funciona o atendimento de urgência no SUS?\n5. Como saber se meu atendimento está agendado?\n6. Telefone para atendimento especializado\n7. Atendimento (Se preferir falar com um atendente)'
                     );
                 } else if (lowerCaseMessage.includes('2') || lowerCaseMessage.includes('atendimento')) {
                     this.userState[message.from] = 'atendimento';
@@ -110,27 +110,54 @@ class WhatsAppAPI {
     
             if (state === 'duvidas') {
                 if (lowerCaseMessage.includes('1')) {
-                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +'Inicialmente, encaminharemos os documentos para a emissão das Notas Fiscais (NFs). Assim que recebermos o retorno da promotora, faremos o envio e agendaremos a entrega. Pedimos gentilmente que aguarde este processo.');
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'Para agendar uma consulta no SUS, você deve procurar uma unidade de saúde mais próxima ou acessar o portal eletrônico do SUS para verificar os procedimentos específicos de agendamento na sua região. Caso precise de ajuda, um atendente pode orientá-lo(a).'
+                    );
                 } else if (lowerCaseMessage.includes('2')) {
-                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +'Infelizmente, não é possível substituir o prêmio por dinheiro. Conforme consta no regulamento: O prêmio não poderá ser distribuído ou convertido, total ou parcialmente em dinheiro, de acordo com o Artigo 15, § 5° do Decreto n° 70.951/1972.');
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'Para ser atendido no SUS, é necessário apresentar documentos como CPF, Cartão SUS, e comprovante de residência. Caso seja uma emergência, o atendimento será imediato.'
+                    );
                 } else if (lowerCaseMessage.includes('3')) {
-                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +'Nossa empresa é totalmente confiável e registrada. Certifique-se que seu nome está listado na aba dos ganhadores no site oficial da promoção.');
-                } else if (lowerCaseMessage.includes('4') || lowerCaseMessage.includes('atendimento')) {
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'Você pode ser atendido em qualquer unidade do SUS, mas é importante verificar a disponibilidade de atendimento na unidade de saúde mais próxima. Dependendo do tipo de serviço, pode ser necessário agendar.'
+                    );
+                } else if (lowerCaseMessage.includes('4')) {
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'O SUS oferece atendimento de urgência 24 horas em unidades de pronto atendimento (UPAs) e hospitais. Em casos graves, o atendimento será imediato.'
+                    );
+                } else if (lowerCaseMessage.includes('5')) {
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'Você pode verificar o status do seu atendimento entrando em contato com a unidade de saúde onde foi realizado o agendamento ou acessando o portal eletrônico do SUS, caso disponível na sua região.'
+                    );
+                } else if (lowerCaseMessage.includes('6') || lowerCaseMessage.includes('telefone') || lowerCaseMessage.includes('atendimento especializado')) {
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'Aqui estão os principais números de telefone para atendimento especializado do SUS:\n' +
+                        '1. **Disque Saúde**: 136 - Para informações gerais sobre o SUS e serviços de saúde.\n' +
+                        '2. **Samu 192**: Para emergências médicas e atendimento de urgência.\n' +
+                        '3. **Defensoria Pública**: 129 - Para questões jurídicas relacionadas ao SUS.\n' +
+                        '4. **Ouvidoria SUS**: 0800 642 9782 - Para fazer denúncias, sugestões ou reclamações sobre os serviços de saúde.\n' +
+                        '5. **Atendimento psicológico e social**: 188 - CVV (Centro de Valorização da Vida), para apoio emocional e prevenção ao suicídio.'
+                    );
+                } else if (lowerCaseMessage.includes('7') || lowerCaseMessage.includes('atendimento')) {
                     await this.client.sendMessage(message.from, 
                         '*Atendimento SUS:*\n\n' +
                         'Em breve, um de nossos atendentes entrará em contato com você.'+
                         '\nObrigado!'
                     );
                 } else {
-                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' + 'Desculpe, não entendi sua dúvida. Responda com o número da sua dúvida.\n1. Quando vou receber meu prêmio?\n2. Posso receber o dinheiro no lugar do prêmio?\n3. Como posso garantir que não é um golpe?\n4. Atendimento (Se preferir falar com um atendente)');
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' +
+                        'Desculpe, não entendi sua dúvida. Responda com o número da sua dúvida.\n1. Como agendar uma consulta no SUS?\n2. Quais são os documentos necessários para ser atendido?\n3. Posso ser atendido em qualquer unidade do SUS?\n4. Como funciona o atendimento de urgência no SUS?\n5. Como saber se meu atendimento está agendado?\n6. Telefone para atendimento especializado\n7. Atendimento (Se preferir falar com um atendente)'
+                    );
                 }
                 return;
             }
-            
+    
             if (state === 'atendimento') {
                 if (lowerCaseMessage.includes('1') || lowerCaseMessage.includes('voltar para dúvidas')) {
                     this.userState[message.from] = 'duvidas';
-                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' + 'Escolha uma dúvida:\n1. Quando vou receber meu prêmio?\n2. Posso receber o dinheiro no lugar do prêmio?\n3. Como posso garantir que não é um golpe?');
+                    await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' + 
+                        'Escolha uma dúvida:\n1. Como agendar uma consulta no SUS?\n2. Quais são os documentos necessários para ser atendido?\n3. Posso ser atendido em qualquer unidade do SUS?'
+                    );
                 } else {
                     await this.client.sendMessage(message.from, 
                         '*Atendimento SUS:*\n\n' +
@@ -140,17 +167,16 @@ class WhatsAppAPI {
                 }
                 return;
             }
-            
     
-            console.log('Mensagem recebida:', message);  
-
+            console.log('Mensagem recebida:', message);
     
             await this.client.sendMessage(message.from, '*Atendimento SUS:*\n\n' + 'Desculpe, não entendi sua mensagem. Poderia reformular ou escolher uma das opções acima?');
-    
+        
         } catch (error) {
             console.error('Erro ao processar mensagem:', error);
         }
     }
+    
     
     
     async ativarBot() {
